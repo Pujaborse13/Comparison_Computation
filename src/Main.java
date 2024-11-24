635f6f48b3f0b7224aa01f3370da5d3b3488f9d6
Puja Borse <107254941+Pujaborse13@users.noreply.github.com>
2024-11-24 22:21:41 +0530

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -6,20 +6,7 @@ public class Main {
 
             System.out.println("Welcome to Line Comparison Computation Program");
 
-        System.out.print("Enter x1: ");
-        double x1 = scanner.nextDouble();
-        System.out.print("Enter y1: ");
-        double y1 = scanner.nextDouble();
-
-        System.out.print("Enter x2: ");
-        double x2 = scanner.nextDouble();
-        System.out.print("Enter y2: ");
-        double y2 = scanner.nextDouble();
-
-        double length = Math.sqrt(Math.pow((x2 - x1), 2) + Math.pow((y2 - y1), 2));
-        System.out.printf("The length of the line is: %.2f\n", length);
-
-        scanner.close();
+       
 
 
     }
