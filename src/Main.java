8369a4e8b4a4ab61f9b3b1b2271d46635f3b1cd6
Puja Borse <pujaborse13@gmail.com>
2024-11-24 21:33:42 +0530

    UC1:Added functionality to calculate the length of a line

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,26 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
 
             System.out.println("Welcome to Line Comparison Computation Program");
 
-        }
+        System.out.print("Enter x1: ");
+        double x1 = scanner.nextDouble();
+        System.out.print("Enter y1: ");
+        double y1 = scanner.nextDouble();
+
+        System.out.print("Enter x2: ");
+        double x2 = scanner.nextDouble();
+        System.out.print("Enter y2: ");
+        double y2 = scanner.nextDouble();
+
+        double length = Math.sqrt(Math.pow((x2 - x1), 2) + Math.pow((y2 - y1), 2));
+        System.out.printf("The length of the line is: %.2f\n", length);
+
+        scanner.close();
+
+
+    }
     }
